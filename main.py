c1c51bb3b63676208ab05b9843d4cc60dea956ce
Alexey <menamet31@gmail.com>
2018-11-20 22:14:06 +0000

    basic layer stats extracted

--- main.py
+++ main.py
@@ -3,13 +3,51 @@ import vgg
 
 
 def main():
-    batch_size = 1
-    height, width = 224, 224
     with tf.Session() as sess:
-        inputs = tf.random_uniform((batch_size, height, width, 3))
+
+        batch_size = 1
+        height, width = 224, 224
+        kernel_depth = 3
+
+        inputs = tf.random_uniform((batch_size, height, width, kernel_depth))
+        # here we are missing the final classification layer
         logits, _ = vgg.vgg_16(inputs)
+
+        # FC can be replaced with CONV, btw official VGG model in TF rep uses only convs
+        target_ops = [op for op in tf.get_default_graph().get_operations()
+                      if op.type == 'Conv2D' or op.type == "MaxPool"]
+
+        for op in target_ops:
+            strides = op.get_attr('strides')
+
+
+            # check if w and h has the same stride
+            assert(strides[1] == strides[2])
+
+
+            #in case of pool stride == kernel size; kernel_depth stays the same
+            kernel_size = strides[1]
+
+            if op.type == "Conv2D":
+                outs = tf.get_default_graph().get_operation_by_name(op.name.replace(op.type, "weights")).outputs[0]
+                dim1, dim2, prev_layer_depth, current_layer_depth = outs.get_shape()
+
+                # check that our conv is square
+                assert(dim1 == dim2)
+                assert(kernel_depth == prev_layer_depth)
+
+                kernel_size = dim1
+                kernel_depth = current_layer_depth
+
+
+            print("{} {} {}x{}x{} stride[{}] padding[{}]"
+                  .format(op.name, op.type, kernel_size, kernel_size, kernel_depth, strides[1], op.get_attr('padding')))
+
+
+
         sess.run(tf.global_variables_initializer())
         output = sess.run(logits)
+
 
 
 if __name__ == "__main__":
